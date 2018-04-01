730e886ddd18921f4c4e0b6b5f7242ac62d6468b
Guijin Ding <dingguijin@gmail.com>
2018-04-01 11:36:18 +0800

    this book

---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = {
+    book: {
+    },
+
+    hooks: {
+        'finish': function() {
+            var _app_uuid = this.book.config.get("app_uuid") || 'a600998e-efff-11e5-9d9f-02287b8c0ebf' ,
+                _ppmessage_url = this.book.config.get("ppmessage_url") || 'https://ppmessage.cn/ppcom/assets/pp-library.min.js';
+                
+            window.ppSettings = {
+                app_uuid:_app_uuid
+            };
+
+            (function(){
+                var w=window,d=document;
+                function l() {
+                    var a=d.createElement('script');
+                    a.type='text/javascript';
+                    a.async=!0;
+                    a.charset='utf-8';
+                    a.src=_ppmessage_url;
+                    var b=d.getElementsByTagName('script')[0];
+                    b.parentNode.insertBefore(a,b)
+                }
+                l();
+            })();
+        }
+    }
+};
